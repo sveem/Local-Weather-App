3dde90c21f18041b76173efb784bbea3548afbd3
Svetlin Mladenov <sveem@users.noreply.github.com>
2018-05-08 16:00:56 -0400

    init: Added Bootstrap and flexbox

--- viewModel.js
+++ viewModel.js
@@ -4,13 +4,16 @@ $(function() {
 // var ViewModel = function() {
   var viewModel = {
     title: 'Weather Application', 
-    location: ko.observableArray(),
-    geoLocation: {},
-    weather: {},
+    geoLocation: { // temp. geolocation
+      latitude: 52.2296756, 
+      longitude: 21.012228699999998
+    },
     city: ko.observable(),
-    country: ko.observable()
+    country: ko.observable(),
+    description: ko.observable(),
+    temperature: ko.observable(),
+    wind: ko.observable()
   }
-
 
   function getGeolocation() {   
       if (navigator.geolocation) { 
@@ -28,15 +31,12 @@ $(function() {
       var url = 'https://fcc-weather-api.glitch.me/api/current?lon=' + viewModel.geoLocation.longitude + '&lat=' + viewModel.geoLocation.latitude;         
        $.get(url, function(data) {
         console.log('Response', data); 
-        viewModel.weather.city = data.name,
-        viewModel.city(data.name),
-        viewModel.country(data.sys.country),
-        // $('#city').text('city: ' + data.name)
-        viewModel.weather.country = data.sys.country,
-        viewModel.weather.description = data.weather.description,
-        viewModel.weather.main = data.weather.main,
-        viewModel.weather.windSpeed = data.wind.speed    
-        console.log('WHEATHER', viewModel.weather);
+        viewModel.city(data.name);
+        viewModel.country(data.sys.country);
+        viewModel.description(data.weather.description);
+        viewModel.temperature(data.main.temp);
+        viewModel.wind(data.wind.speed);
+        console.log('ViewModel', viewModel);
       })
     });
   
