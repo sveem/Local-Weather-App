43e7342e7f6e03b16ef8c3e6cca3c456317a96eb
Svetlin Mladenov <sveem@users.noreply.github.com>
2018-05-08 17:29:19 -0400

    init: Background image, showing weather description

--- viewModel.js
+++ viewModel.js
@@ -1,9 +1,8 @@
 
 $(function() {
 
-// var ViewModel = function() {
   var viewModel = {
-    title: 'Weather Application', 
+    title: 'Local Weather App', 
     geoLocation: { // temp. geolocation
       latitude: 52.2296756, 
       longitude: 21.012228699999998
@@ -16,33 +15,28 @@ $(function() {
   }
 
   function getGeolocation() {   
-      if (navigator.geolocation) { 
-        return navigator.geolocation.getCurrentPosition(function(position) { 
-        viewModel.geoLocation.latitude = position.coords.latitude;
-        viewModel.geoLocation.longitude = position.coords.longitude;
-        callWeatherAPI();
-      })
-      } else {
-       console.log("Geolocation is not supported by this browser.");
-      }
+    if (navigator.geolocation) { 
+      return navigator.geolocation.getCurrentPosition(function (position) { 
+      viewModel.geoLocation.latitude = position.coords.latitude;
+      viewModel.geoLocation.longitude = position.coords.longitude;
+      callWeatherAPI();
+    });
+      } 
     }
 
   var callWeatherAPI = ko.pureComputed(function() {
-      var url = 'https://fcc-weather-api.glitch.me/api/current?lon=' + viewModel.geoLocation.longitude + '&lat=' + viewModel.geoLocation.latitude;         
-       $.get(url, function(data) {
-        console.log('Response', data); 
+    var url = 'https://fcc-weather-api.glitch.me/api/current?lon=' + viewModel.geoLocation.longitude + '&lat=' + viewModel.geoLocation.latitude;         
+      $.get(url, function(data) {
+        console.log('Response', data); ; 
         viewModel.city(data.name);
         viewModel.country(data.sys.country);
-        viewModel.description(data.weather.description);
+        viewModel.description(data.weather[0].description);
         viewModel.temperature(data.main.temp);
         viewModel.wind(data.wind.speed);
-        console.log('ViewModel', viewModel);
       })
     });
   
     getGeolocation();
-
-// };
 
 // Pass the viewModel as param
 ko.applyBindings(viewModel)
